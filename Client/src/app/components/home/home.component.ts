5a42efd178b1bce5b914c69f050824d093c81206
Luquebiqueno <dikizekopires@gmail.com>
2024-02-22 22:13:44 -0300

    ajustes publicação

--- Client/src/app/components/home/home.component.ts
+++ Client/src/app/components/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 import { PublicacaoService } from '../../services/publicacao.service';
 import { Publicacao } from '../../models/publicacao';
 
 @Component({
 	selector: 'app-home',
 	standalone: true,
-	imports: [],
+	imports: [
+		MatCardModule,
+		MatButtonModule,
+		MatIconModule,
+		MatDividerModule
+	],
 	templateUrl: './home.component.html',
 	styleUrl: './home.component.scss'
 })
